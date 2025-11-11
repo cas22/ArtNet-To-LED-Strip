c9c88a015e6d2d55c7b9d681cb64529a08572765
Carlos SM <38817478+cas22@users.noreply.github.com>
2025-11-11 23:53:36 +0100

    Formatted code and rebuilt all enviroments

--- src/main.cpp
+++ src/main.cpp
@@ -5,34 +5,40 @@
 	which is not divisible by 3, leaving us with 2 unused channels.
 	Therefore, we work with 510 channels and due to how the ArtNet library works, we subtract one since
 	the vector starts at 0.
-	
+
 	510 / 3 = 170, which is multiplied by the universe to calculate which LED we are at when we are in a
 	universe != 0.
 */
 
-#if GROUP_LED == 1 
-void onArtNetFrame(const uint8_t* data, const uint16_t size, const art_net::art_dmx::Metadata& metadata, const art_net::RemoteInfo& remote) {
-	int ledpos = metadata.universe*170;
-	for (size_t i = 0; i <= 509 && ledpos < NUM_PIXELS; i += 3) {
-		strip.SetPixelColor(ledpos, RgbColor(data[i], data[i+1], data[i+2]));
+#if GROUP_LED == 1
+void onArtNetFrame(const uint8_t *data, const uint16_t size, const art_net::art_dmx::Metadata &metadata, const art_net::RemoteInfo &remote)
+{
+	int ledpos = metadata.universe * 170;
+	for (size_t i = 0; i <= 509 && ledpos < NUM_PIXELS; i += 3)
+	{
+		strip.SetPixelColor(ledpos, RgbColor(data[i], data[i + 1], data[i + 2]));
 		ledpos++;
 	}
-    strip.Show();
+	strip.Show();
 }
 #else
-void onArtNetFrame(const uint8_t* data, const uint16_t size, const art_net::art_dmx::Metadata& metadata, const art_net::RemoteInfo& remote) {
-	int ledpos = metadata.universe*(170);
-	for (size_t i = 0; i <= 509 && ledpos < NUM_PIXELS; i += 3) {
-		for(size_t j=1; j <= GROUP_LED; j++){
-			strip.SetPixelColor(ledpos, RgbColor(data[i], data[i+1], data[i+2]));
+void onArtNetFrame(const uint8_t *data, const uint16_t size, const art_net::art_dmx::Metadata &metadata, const art_net::RemoteInfo &remote)
+{
+	int ledpos = metadata.universe * (170);
+	for (size_t i = 0; i <= 509 && ledpos < NUM_PIXELS; i += 3)
+	{
+		for (size_t j = 1; j <= GROUP_LED; j++)
+		{
+			strip.SetPixelColor(ledpos, RgbColor(data[i], data[i + 1], data[i + 2]));
 			ledpos++;
 		}
 	}
-    strip.Show();
+	strip.Show();
 }
 #endif
 
-void setup() {
+void setup()
+{
 	Serial.begin(115200);
 	Serial.println("[Info] Current version: " + String(VERSION));
 
@@ -40,12 +46,12 @@ void setup() {
 	Serial.println("[NeoPixelBus] Starting using pin " + String(DATA_PIN));
 	strip.Begin();
 	Serial.printf("[NeoPixelBus] Started %d pixels", NUM_PIXELS);
-  strip.Show();
+	strip.Show();
 
-  setup_network();
+	setup_network();
 
 	Serial.println("\n[OTA] Checking if updates are available");
-  checkAndUpdate();
+	checkAndUpdate();
 
 	Serial.println("\n[ArtNet] setArtPollReplyConfig");
 	artnet.setArtPollReplyConfigShortName(ARTNET_ShortName);
@@ -62,23 +68,28 @@ void setup() {
 	Serial.printf("[Info] Free Heap %d out of %d", ESP.getFreeHeap(), ESP.getHeapSize());
 }
 
-void loop() {	
+void loop()
+{
 	artnet.parse();
 }
 
-
 /*----- LED Strip Helpers -----*/
-void fill_ledstrip(RgbColor color){
-	for(size_t i = 0; i<NUM_PIXELS; i++){
+void fill_ledstrip(RgbColor color)
+{
+	for (size_t i = 0; i < NUM_PIXELS; i++)
+	{
 		strip.SetPixelColor(i, color);
 	}
 	strip.Show();
 }
 
-void stars_ledstrip(uint8_t colorMax, uint8_t starFrequency){
-	for(size_t i = 0; i<NUM_PIXELS; i++){
-		if(random(0,starFrequency) == starFrequency) {
-			strip.SetPixelColor(i, RgbColor(random(0,colorMax)));
+void stars_ledstrip(uint8_t colorMax, uint8_t starFrequency)
+{
+	for (size_t i = 0; i < NUM_PIXELS; i++)
+	{
+		if (random(0, starFrequency) == starFrequency)
+		{
+			strip.SetPixelColor(i, RgbColor(random(0, colorMax)));
 		}
 	}
 	strip.Show();
@@ -86,126 +97,147 @@ void stars_ledstrip(uint8_t colorMax, uint8_t starFrequency){
 
 /*----- Network -----*/
 #ifdef wt32eth01
-void setup_network() {
-  uint8_t animBrightness = 0;
+void setup_network()
+{
+	uint8_t animBrightness = 0;
 	Network.onEvent(onEvent);
 	ETH.begin();
-	while(!eth_connected) {
+	while (!eth_connected)
+	{
 		delay(20);
 		fill_ledstrip(RgbColor(animBrightness));
-		animBrightness = (animBrightness == 250) ? 0 : animBrightness+1;
+		animBrightness = (animBrightness == 250) ? 0 : animBrightness + 1;
 	}
-	fill_ledstrip(RgbColor(0,255,0));
+	fill_ledstrip(RgbColor(0, 255, 0));
 	Serial.println("\n[ETH] Connected to the network");
 }
 
-void onEvent(arduino_event_id_t event) {
-  switch (event) {
-  case ARDUINO_EVENT_ETH_START:
-    Serial.println("[ETH] Started");
-    ETH.setHostname(ETH_HOST_NAME);
-    break;
-  case ARDUINO_EVENT_ETH_CONNECTED:
-    Serial.println("[ETH] Connected");
-    break;
-  case ARDUINO_EVENT_ETH_GOT_IP:
-    Serial.println("[ETH] Got IP " + String(ETH.localIP()));
-    eth_connected = true;
-    break;
-  case ARDUINO_EVENT_ETH_DISCONNECTED:
-    Serial.println("[ETH] Disconnected");
-    eth_connected = false;
-    break;
-  case ARDUINO_EVENT_ETH_STOP:
-    Serial.println("[ETH] Stopped");
-    eth_connected = false;
-    break;
-  default:
-    break;
-  }
+void onEvent(arduino_event_id_t event)
+{
+	switch (event)
+	{
+	case ARDUINO_EVENT_ETH_START:
+		Serial.println("[ETH] Started");
+		ETH.setHostname(ETH_HOST_NAME);
+		break;
+	case ARDUINO_EVENT_ETH_CONNECTED:
+		Serial.println("[ETH] Connected");
+		break;
+	case ARDUINO_EVENT_ETH_GOT_IP:
+		Serial.println("[ETH] Got IP " + String(ETH.localIP()));
+		eth_connected = true;
+		break;
+	case ARDUINO_EVENT_ETH_DISCONNECTED:
+		Serial.println("[ETH] Disconnected");
+		eth_connected = false;
+		break;
+	case ARDUINO_EVENT_ETH_STOP:
+		Serial.println("[ETH] Stopped");
+		eth_connected = false;
+		break;
+	default:
+		break;
+	}
 }
 #endif
 
 #ifdef esp32
-    void setup_network() {
-        uint8_t animBrightness = 0;
-        WiFi.begin(ssid, pwd);
-        WiFi.config(ip, gateway, subnet_mask);
-        while(WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        fill_ledstrip(RgbColor(animBrightness));
-        animBrightness = (animBrightness == 250) ? 0 : animBrightness+1;
-        }
-        fill_ledstrip(RgbColor(0,255,0));
-        Serial.printf("\n[WiFi] Connected to the network with IP: %d", WiFi.localIP());
-    }
+void setup_network()
+{
+	uint8_t animBrightness = 0;
+	WiFi.begin(ssid, pwd);
+	WiFi.config(ip, gateway, subnet_mask);
+	while (WiFi.status() != WL_CONNECTED)
+	{
+		delay(500);
+		fill_ledstrip(RgbColor(animBrightness));
+		animBrightness = (animBrightness == 250) ? 0 : animBrightness + 1;
+	}
+	fill_ledstrip(RgbColor(0, 255, 0));
+	Serial.printf("\n[WiFi] Connected to the network with IP: %d", WiFi.localIP());
+}
 #endif
 
 /*----- OTA -----*/
-void checkAndUpdate() {
+void checkAndUpdate()
+{
 	HTTPClient http;
 	// Step 1: Check version
 	http.begin(version_url);
 	int versionCode = http.GET();
-	if (versionCode == HTTP_CODE_OK) {
+	if (versionCode == HTTP_CODE_OK)
+	{
 		String serverVersion = http.getString();
 		serverVersion.trim();
 
 		Serial.println("[OTA] Current version: " + String(VERSION));
 		Serial.println("[OTA] Server version: " + serverVersion);
 
-		if (serverVersion == VERSION) {
+		if (serverVersion == VERSION)
+		{
 			Serial.println("[OTA] Firmware is up to date.");
 			http.end();
 			return;
 		}
-		else {
+		else
+		{
 			Serial.println("[OTA] New firmware available. Starting OTA update...");
 		}
 	}
-	else {
+	else
+	{
 		Serial.println("[OTA] Failed to fetch version info. HTTP Code: " + String(versionCode));
 		http.end();
 		return;
 	}
 	http.end();
-	
+
 	// Step 2: Get Firmware
 	http.begin(firmware_url);
 	int httpCode = http.GET();
 
-	if (httpCode == HTTP_CODE_OK) {
+	if (httpCode == HTTP_CODE_OK)
+	{
 		int contentLength = http.getSize();
-		NetworkClient* stream = http.getStreamPtr();
+		NetworkClient *stream = http.getStreamPtr();
 		fill_ledstrip(RgbColor(255, 255, 0));
-		if (Update.begin(contentLength)) {
+		if (Update.begin(contentLength))
+		{
 			size_t written = Update.writeStream(*stream);
-			if (written == contentLength) {
+			if (written == contentLength)
+			{
 				Serial.println("[OTA] Written : " + String(written) + " successfully");
 			}
-			else {
+			else
+			{
 				Serial.println("[OTA] Written only : " + String(written) + "/" + String(contentLength));
 			}
 
-			if (Update.end()) {
-				if (Update.isFinished()) {
+			if (Update.end())
+			{
+				if (Update.isFinished())
+				{
 					Serial.println("[OTA] Update successfully completed. Rebooting...");
 					ESP.restart();
 				}
-				else {
+				else
+				{
 					Serial.println("[OTA] Update not finished? Something went wrong.");
 				}
 			}
-			else {
+			else
+			{
 				Serial.printf("[OTA] Error Occurred. Error #: %d\n", Update.getError());
 			}
 		}
-		else {
+		else
+		{
 			Serial.println("[OTA] Not enough space to begin");
 			fill_ledstrip(RgbColor(255, 0, 0));
 		}
 	}
-	else {
+	else
+	{
 		Serial.println("[OTA] Firmware not available. HTTP Code: " + String(httpCode));
 	}
 	http.end();
